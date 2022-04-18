6907e7b96752327775af22fa14a979ab14754a84
Maximiliano Firtman <firtman@gmail.com>
2022-04-18 11:39:05 -0300

    first commit

---
+++ webapp/app.js
@@ -0,0 +1,30 @@
+window.addEventListener("DOMContentLoaded", event => {
+    document.querySelector("button").addEventListener("click", event => {
+        let name = document.querySelector("#name").value;
+        let email = document.querySelector("#email").value;
+        let feedback = document.querySelector("textarea").value;
+        if (document.querySelector("form").checkValidity()) {
+            // TODO: Send the data to the server
+            console.log(`Name: ${name} Email: ${email} Feedback: ${feedback}`);
+            document.querySelector("#name").value = "";
+            document.querySelector("#email").value = "";
+            document.querySelector("textarea").value = "";
+            showDialog("We've received your feedback. Thanks!")
+        } else {
+            showDialog("Please enter a valid name and email address.")
+        }
+    })
+});
+
+function showDialog(message) {
+    if ('native' in window) {
+        native.alert(message);
+    } else {
+        alert(message)
+    }
+}
+
+function injectFormData(name, email="") {
+    document.querySelector("#name").value = name;
+    document.querySelector("#email").value = email;
+}
